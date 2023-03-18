d54a2e371b4da9ca4799185c469f9450c8fa0a68
kkalin-ua <94871840+kkalin-ua@users.noreply.github.com>
2023-03-18 14:34:33 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,41 @@
+// Напиши скрипт, який після натискання кнопки «Start», раз на секунду змінює колір фону 
+// <body> на випадкове значення, використовуючи інлайн стиль. Натисканням на кнопку 
+// «Stop» зміна кольору фону повинна зупинятися.
 
+// Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів. 
+// Зроби так, щоб доки зміна теми запущена, кнопка «Start» була неактивною (disabled).
+
+
+const startBtnEl = document.querySelector('button[data-start]');
+console.log(startBtnEl);
+
+const stopBtnEl = document.querySelector('button[data-stop]');
+console.log(stopBtnEl);
+stopBtnEl.disabled = true;
+
+startBtnEl.addEventListener('click', onStartBtnClick);
+
+function onStartBtnClick (event) {
+  
+  document.body.style.background = getRandomHexColor();
+  startBtnEl.disabled = true;
+  stopBtnEl.disabled = false;
+
+  let intervalId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+
+
+  stopBtnEl.addEventListener('click', onStopBtnClick);
+
+  function onStopBtnClick (event) {
+    clearInterval(intervalId);
+    startBtnEl.disabled = false;
+    stopBtnEl.disabled = true;
+  }
+}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
---
+++ src/js/02-timer.js
@@ -0,0 +1,173 @@
+// Виконуй це завдання у файлах 02-timer.html і 02-timer.js. Напиши скрипт 
+// таймера, який здійснює зворотний відлік до певної дати. Такий таймер може 
+// використовуватися у блогах та інтернет-магазинах, сторінках реєстрації подій,
+//  під час технічного обслуговування тощо. Подивися демо-відео роботи таймера.
+
+
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+const dateTimeInputEl = document.querySelector('#datetime-picker');
+const btnStartEl = document.querySelector('button[data-start]');
+
+
+// console.log(dateTimeInputEl);
+// console.log(btnStartEl);
+// console.log(dataDaysEl);
+// console.log(dataHoursEl);
+// console.log(dataNinutesEl);
+// console.log(dataSecondsEl);
+
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+
+
+
+
+
+flatpickr('#datetime-picker', {
+  enableTime: true,
+  allowInput: true,
+  dateFormat: "m/d/Y h:i K",
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+});
+
+
+
+
+let options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    
+    // console.log(btnStartEl);
+    let dateNum = selectedDates[0]-0;
+    let curentDate = Date.now()
+    // console.log(dateNum);
+    // console.log(Date.now())
+    
+  
+    // console.log(selectedDates[0]-0);
+    
+
+
+    if (selectedDates[0] < new Date()) {
+      
+      console.log('Please choose a date in the future');
+      return;
+    }
+
+
+    btnStartEl.addEventListener('click', onBtnClick);
+
+    function onBtnClick(event) {
+      event.preventDefault();
+    
+      console.log(`Вибраная дата: ${dateNum-0}`);
+      // console.log(options.defaultDate-0);
+      // console.log(`Текущая дата: ${curentDate-0}`);
+      // console.log(dateDelta);
+      let dateDelta = (dateNum - curentDate);
+      // let { days, hours, minutes, seconds } = convertMs(dateDelta)
+      console.log(`Разница дат: ${dateDelta-0}`);
+      // console.log(`${pad(days)} ${pad(hours)}:${pad(minutes)}:${pad(seconds)}`);
+
+
+
+      let intervalId = setInterval(() => {
+        // console.log(`Разница дат: ${dateDelta-0}`);
+      // console.log(dateDelta);
+      dateDelta = dateNum - Date.now();
+      let { days, hours, minutes, seconds } = convertMs(dateDelta);
+
+      // console.log(`${pad(days)} ${pad(hours)}:${pad(minutes)}:${pad(seconds)}`)
+      // console.log(convertMs(deltaDate));
+
+
+      const dataDaysEl = document.querySelector('span[data-days]');
+      dataDaysEl.innerHTML = days;
+      const dataHoursEl = document.querySelector('span[data-hours]');
+      dataHoursEl.innerHTML = hours;
+      const dataNinutesEl = document.querySelector('span[data-minutes]');
+      dataNinutesEl.innerHTML = minutes;
+      const dataSecondsEl = document.querySelector('span[data-seconds]');
+      dataSecondsEl.innerHTML = seconds;
+
+    }, 1000);
+
+
+      if (dateDelta < 0) {
+        clearInterval(intervalId);
+      }
+    }
+  },
+};
+
+
+flatpickr('#datetime-picker', { ...options });
+
+// const dataDaysEl2 = document.querySelector('span[data-days]');
+// console.log(dataDaysEl2.innerHTML);
+
+
+
+// function pad(value) {
+//   return String(value).padStart(2, '0');
+// }
+
+
+
+
+// dateTimeInputEl.addEventListener('input', onDateInput);
+// function onDateInput(event) {
+//   const sss = event.target.value;
+//   // console.log(sss-0);
+// }
+
+
+// const curentDate = Date.now()
+// const timer = {
+//   start() {
+//     const startTime = Date.now();
+//     // console.log(startTime)
+//       setInterval(() => {
+//         const curentDate = Date.now();
+//         // console.log(curentDate)
+//         const deltaDate = curentDate - startTime;
+//         const { days, hours, minutes, seconds } = convertMs(deltaDate);
+
+//         console.log(`${pad(days)} ${pad(hours)}:${pad(minutes)}:${pad(seconds)}`);
+//         // console.log(convertMs(deltaDate));
+//       }, 1000);
+//   }
+// }
+
+
+
+// timer.start();
+// console.log(timer.start);
+
+
+
+
+
+
